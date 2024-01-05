f5dc75ceec2951f30d2bb04700047795dd7881d3
2ndborn <131536159+2ndborn@users.noreply.github.com>
2024-01-05 08:02:35 +0000

    Final commit, changing debug to false.

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def delete_review(review_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
